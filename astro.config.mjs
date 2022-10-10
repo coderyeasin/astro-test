c04234f17b5344122c977dfa7c037ac1fed877ef
coderyeasin <yeasin3233@gmail.com>
2022-10-10 17:52:10 +0600

    cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next()],
-  // vite: {
+  integrations: [tailwind(), astroI18next(), mdx()] // vite: {
   //   ssr: {
   //     external: ["svgo"],
   //   },
   // },
+
 });
