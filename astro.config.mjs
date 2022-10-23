d351e323760ea11d778145560fae460be7e191a9
coderyeasin <yeasin3233@gmail.com>
2022-10-23 18:01:45 +0600

    img_fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 import mdx from "@astrojs/mdx";
 
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    // config:{ applyBaseStyles: false}
-  }), astroI18next(), mdx()]
+  integrations: [tailwind({// config:{ applyBaseStyles: false}
+  }), astroI18next(), mdx(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })]
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@
 module.exports = {
 	content: [
 	  "./public/**/*.html",
-	  "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}",
+	  "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue,md,mdx}",
 	],
 	important: true,
 	theme: {
