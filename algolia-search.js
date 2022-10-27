ed395a7cf2668eafbc8afc69ca451447594dd67c
coderyeasin <yeasin3233@gmail.com>
2022-10-27 16:04:17 +0600

    post

--- algolia-search.js
+++ algolia-search.js
@@ -1,15 +1,64 @@
+const dotenv = require('dotenv')
+const fs = require('fs');
+const path = require('path');
 const algoliasearch = require('algoliasearch');
-const client = algoliasearch('AXQFNLOTUN', '65e05d13c4d9893af2649ac8a2799a7c');
-const index = client.initIndex('netlify_f7c3c02f-9e4e-41fc-a0ba-3b2b050640c8_algo-test_all');
+const matter = require('gray-matter');
 
-const actress = [
-    {
-        name: "Lopez",
-        rating: 4.5
-    },
-    {
-        name:"Shakira",
-        rating: 5
+try {
+    dotenv.config()
+    if(!process.env.ASTRO_ALGOLIA_APP_ID){
+      throw new Error('api id not define') 
     }
-];
-index.addObjects(actress)
+    if(!process.env.ASTRO_ALGOLIA_API_KEY){
+        throw new Error('api key not define') 
+    }
+} catch (error) {
+    console.log(error);
+    process.exit(1)
+}
+
+const CONTENT_PATH = path.join(process.cwd(), "src/pages/posts/en");
+const contentFilePaths = fs.readdirSync(CONTENT_PATH).filter((path) => /\.{md,mx}?$/.test(path))
+
+async function getAllBlogPosts() {
+    const articles = contentFilePaths.map((filePath) => {
+        const source = fs.readFileSync(path.join(CONTENT_PATH, filePath))
+        const {content, data} = matter(source)
+        return{
+            content,
+            data,
+            filePath
+        }
+    })
+    return articles
+}
+
+function transformPostsToSearchObjects(articles){
+    const transform = articles.map(article => {
+        return {
+            objectId: article.data.title,
+            title: article.data.title,
+            description: article.data.excerpt,
+            slug:article.filePath,
+            date: article.data.date
+        };
+    });
+    return transform;
+}
+
+(async function(){
+    dotenv.config();
+    try{
+        const articles = await getAllBlogPosts();
+        const transform = transformPostsToSearchObjects(articles);
+
+        const client = algoliasearch(process.env.ASTRO_ALGOLIA_APP_ID, process.env.ASTRO_ALGOLIA_API_KEY);
+        const index = client.initIndex("netlify_f7c3c02f-9e4e-41fc-a0ba-3b2b050640c8_algo-test_all");
+        
+        const algoliaResponse = await index.saveObjects(transform);
+        console.log(`successfully added ${algoliaResponse.objectId.length} records`);
+    }
+    catch(err){
+        console.log(err);
+    }
+})
