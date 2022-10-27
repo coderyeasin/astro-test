7c488cebebccc2ef35af1919c14dc6ca49145e95
coderyeasin <yeasin3233@gmail.com>
2022-10-27 13:36:05 +0600

    tst_recrd

--- algolia-search.js
+++ algolia-search.js
@@ -1,7 +1,7 @@
 const algoliasearch = require('algoliasearch');
 
-const client = algoliasearch('YourApplicationID', 'YourAdminAPIKey');
-const index = client.initIndex('your_index_name');
+const client = algoliasearch('AXQFNLOTUN', '65e05d13c4d9893af2649ac8a2799a7c');
+const index = client.initIndex('netlify_f7c3c02f-9e4e-41fc-a0ba-3b2b050640c8_algo-test_all');
 
 const objects = [
   {
