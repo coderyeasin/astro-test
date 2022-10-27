867586497cc44fa830bb1aba260250db6f05c41b
coderyeasin <yeasin3233@gmail.com>
2022-10-27 15:25:23 +0600

    push_data

--- algolia-search.js
+++ algolia-search.js
@@ -1,28 +1,15 @@
 const algoliasearch = require('algoliasearch');
-
 const client = algoliasearch('AXQFNLOTUN', '65e05d13c4d9893af2649ac8a2799a7c');
 const index = client.initIndex('netlify_f7c3c02f-9e4e-41fc-a0ba-3b2b050640c8_algo-test_all');
 
-const objects = [
-  {
-    objectID: 1,
-    name: 'Foo',
-  },
+const actress = [
+    {
+        name: "Lopez",
+        rating: 4.5
+    },
+    {
+        name:"Shakira",
+        rating: 5
+    }
 ];
-
-index
-  .saveObjects(objects)
-  .then(({ objectIDs }) => {
-    console.log(objectIDs);
-  })
-  .catch(err => {
-    console.log(err);
-  });
-  index
-  .search('Fo')
-  .then(({ hits }) => {
-    console.log(hits);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+index.addObjects(actress)
