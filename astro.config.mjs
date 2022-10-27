cbc69a0105d2ecefb942619e4641b7f74c2e09e9
coderyeasin <yeasin3233@gmail.com>
2022-10-27 17:39:16 +0600

    site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
-
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({// config:{ applyBaseStyles: false}
   }), astroI18next(), mdx(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  }), sitemap()]
+  }),sitemap()],
+  site:'https://idyllic-cassata-b3391f.netlify.app'
 });
