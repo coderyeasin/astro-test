5a037c70d5f8943ff9344addd5b63b2829f02e96
coderyeasin <yeasin3233@gmail.com>
2022-10-27 17:31:34 +0600

    rm_db

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import astroI18next from "astro-i18next";
 import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({// config:{ applyBaseStyles: false}
   }), astroI18next(), mdx(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  })]
+  }), sitemap()]
 });
