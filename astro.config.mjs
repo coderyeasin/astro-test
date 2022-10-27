bcf73f5623e2ce59239e8ef743b9a90c5080198b
coderyeasin <yeasin3233@gmail.com>
2022-10-27 17:24:47 +0600

    polish_new_app

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 import mdx from "@astrojs/mdx";
-
 import image from "@astrojs/image";
 
 // https://astro.build/config
