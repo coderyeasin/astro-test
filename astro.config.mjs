e433ba4ddfde2e1aec8ce5a0db1f981a1306c4a9
coderyeasin <yeasin3233@gmail.com>
2022-10-29 21:46:41 +0600

    add reord org

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,19 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 import mdx from "@astrojs/mdx";
-
 import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({// config:{ applyBaseStyles: false}
-  }), astroI18next(), mdx(), image({
+  // site: '',
+  integrations: [
+    tailwind({
+      // config:{ applyBaseStyles: false}
+  }), 
+  astroI18next(), mdx(), 
+  image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  })]
+  }), 
+  sitemap()]
 });
